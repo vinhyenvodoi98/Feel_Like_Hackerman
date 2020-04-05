1e31ac4b41865658b92c65c30c5711cbb51aa1b9
Doannv97 <43171353+Doannv97@users.noreply.github.com>
2020-04-05 11:15:31 +0700

    add Error

---
+++ src/components/Error/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Error.css';
+
+class Error extends Component {
+  render() {
+    return (
+      <div className='card error'>
+        <div>パスワードが間違っていますもう一度お試しください</div>
+      </div>
+    );
+  }
+}
+
+export default Error;
